6215f0c2a91552aa8008adf192b3a6865adbe0dd
nrenner <nrenner0211@gmail.com>
2023-03-23 15:24:34 -0400

    setTimeout added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Carousel from './components/Carousel';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+
+const slides = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className='max-w-lg'>
+        <Carousel autoslide={true} autoSlideInterval={3000}>
+          {slides.map((s) => (
+            <img key={slides.indexOf} src={s} alt='artwork'/>
+          ))}
+        </Carousel>
+      </div>
+    </main>
   );
 }
 
